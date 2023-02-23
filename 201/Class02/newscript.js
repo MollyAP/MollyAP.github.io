b69624be6d7acf541f89611d946e8b241b42c008
Mollyphobia <123973226+Mollyphobia@users.noreply.github.com>
2023-02-23 11:13:44 -0600

    fix

--- 201/Class02/newscript.js
+++ 201/Class02/newscript.js
@@ -1,12 +1,12 @@
-function whodat() {
-    myname = prompt("What is your name?");
-    let name = (myname);
-    alert(name + "? Interesting, alright then.");
-    document.getElementById('name').innerHTML = "<span id='name'>"+name+"</span>";
-}
-whodat();
+// function whodat() {
+//     myname = prompt("What is your name?");
+//     let name = (myname);
+//     alert(name + "? Interesting, alright then.");
+//     document.getElementById('name').innerHTML = "<span id='name'>"+name+"</span>";
+// }
+// whodat();
 function show_prompt1() {
-    var a = prompt ("Help I forgot :(");
+    var a = prompt("Help I forgot :(");
 
     document.getElementById('promptreturn1').innerHTML = a;
     if (a == "No") {
@@ -15,27 +15,27 @@ function show_prompt1() {
     else if (a == "Yes") {
         document.getElementById('promptreturn1').innerHTML = "CORRECT... I THINK?";
     }
-    else{
-        document.getElementById('promptreturn1').innerHTML = "<span id='promptretval'>" + a.toLocaleUpperCase() + " IS NOT A VALID ANSWER "+ myname.toLocaleUpperCase() +" SAY YES OR NO WITH CORRECT CAPITALIZATION!</span>";
+    else {
+        document.getElementById('promptreturn1').innerHTML = "<span id='promptretval'>" + a.toLocaleUpperCase() + " IS NOT A VALID ANSWER " + myname.toLocaleUpperCase() + " SAY YES OR NO WITH CORRECT CAPITALIZATION!</span>";
     }
 }
 
 function show_prompt2() {
-    var b = prompt ("Do I?");
+    var b = prompt("Do I?");
 
     document.getElementById('promptreturn2').innerHTML = b;
     if (b == "No") {
         document.getElementById('promptreturn2').innerHTML = "WRONG, YOU DON'T KNOW ME.";
     }
     else if (b == "Yes") {
-        document.getElementById('promptreturn2').innerHTML = "CORRECT, BUT I WON'T DRAW FOR YOU, "+myname.toLocaleUpperCase();
+        document.getElementById('promptreturn2').innerHTML = "CORRECT, BUT I WON'T DRAW FOR YOU, " + myname.toLocaleUpperCase();
     }
-    else{
-        document.getElementById('promptreturn2').innerHTML = "<span id='promptretval'>" + b.toLocaleUpperCase() + " IS NOT A VALID ANSWER "+ myname.toLocaleUpperCase() +" SAY YES OR NO WITH CORRECT CAPITALIZATION!</span>";
+    else {
+        document.getElementById('promptreturn2').innerHTML = "<span id='promptretval'>" + b.toLocaleUpperCase() + " IS NOT A VALID ANSWER " + myname.toLocaleUpperCase() + " SAY YES OR NO WITH CORRECT CAPITALIZATION!</span>";
     }
 }
 function show_prompt3() {
-    var c = prompt ("I hope the answer is Yes.");
+    var c = prompt("I hope the answer is Yes.");
 
     document.getElementById('promptreturn3').innerHTML = c;
     if (c == "No") {
@@ -44,83 +44,103 @@ function show_prompt3() {
     else if (c == "Yes") {
         document.getElementById('promptreturn3').innerHTML = "WRONG, BUT I WISH IT WAS CORRECT...";
     }
-    else{
-        document.getElementById('promptreturn3').innerHTML = "<span id='promptretval'>" + c.toLocaleUpperCase() + " IS NOT A VALID ANSWER "+ myname.toLocaleUpperCase() +" SAY YES OR NO WITH CORRECT CAPITALIZATION!</span>";
+    else {
+        document.getElementById('promptreturn3').innerHTML = "<span id='promptretval'>" + c.toLocaleUpperCase() + " IS NOT A VALID ANSWER " + myname.toLocaleUpperCase() + " SAY YES OR NO WITH CORRECT CAPITALIZATION!</span>";
     }
 }
 function show_prompt4() {
-    var d = prompt ("If u say yes you HAVE to help me FINANCIALLY in getting one.");
+    var d = prompt("If u say yes you HAVE to help me FINANCIALLY in getting one.");
 
     document.getElementById('promptreturn4').innerHTML = d;
     if (d == "No") {
-        document.getElementById('promptreturn4').innerHTML = "WRONG! HOW ABOUT NEXT TIME YOU PICK THE CORRECT ANSWER "+myname.toLocaleUpperCase()+"!!";
+        document.getElementById('promptreturn4').innerHTML = "WRONG! HOW ABOUT NEXT TIME YOU PICK THE CORRECT ANSWER " + myname.toLocaleUpperCase() + "!!";
     }
     else if (d == "Yes") {
         document.getElementById('promptreturn4').innerHTML = "CORRECT! NOW BUY ME ONE!!";
     }
-    else{
-        document.getElementById('promptreturn4').innerHTML = "<span id='promptretval'>" + d.toLocaleUpperCase() + " IS NOT A VALID ANSWER "+ myname.toLocaleUpperCase() +" SAY YES OR NO WITH CORRECT CAPITALIZATION!</span>";
+    else {
+        document.getElementById('promptreturn4').innerHTML = "<span id='promptretval'>" + d.toLocaleUpperCase() + " IS NOT A VALID ANSWER " + myname.toLocaleUpperCase() + " SAY YES OR NO WITH CORRECT CAPITALIZATION!</span>";
     }
 }
 function show_prompt5() {
-    var e = prompt ("Molly IS pretty smart so idk...");
+    var e = prompt("Molly IS pretty smart so idk...");
 
     document.getElementById('promptreturn5').innerHTML = e;
     if (e == "No") {
-        document.getElementById('promptreturn5').innerHTML = "CORRECT! IT'S OFFICIAL!! "+myname.toLocaleUpperCase()+" CAN READ!!! CONGRATS!!!!";
+        document.getElementById('promptreturn5').innerHTML = "CORRECT! IT'S OFFICIAL!! " + myname.toLocaleUpperCase() + " CAN READ!!! CONGRATS!!!!";
     }
     else if (e == "Yes") {
-        document.getElementById('promptreturn5').innerHTML = "WRONG, YOU LITERALLY COULD HAVE LOOKED TO THE SIDE "+myname.toLocaleUpperCase()+"!!";
+        document.getElementById('promptreturn5').innerHTML = "WRONG, YOU LITERALLY COULD HAVE LOOKED TO THE SIDE " + myname.toLocaleUpperCase() + "!!";
     }
-    else{
-        document.getElementById('promptreturn5').innerHTML = "<span id='promptretval'>" + e.toLocaleUpperCase() + " IS NOT A VALID ANSWER "+ myname.toLocaleUpperCase() +" SAY YES OR NO WITH CORRECT CAPITALIZATION!</span>";
-    }
-}
-
-
-function thisFunc() {
-
-    userAns = prompt("Let's here em");
-
-    let thisArray = [10, 6, 4, 12, "Hello"];
-
-
-        if (userAns != thisArray){
-            alert("WRONG");
-            userAns = prompt("Please guess a number again!");
-        }
-    else if(userAns == thisArray){
-            alert("YES");
+    else {
+        document.getElementById('promptreturn5').innerHTML = "<span id='promptretval'>" + e.toLocaleUpperCase() + " IS NOT A VALID ANSWER " + myname.toLocaleUpperCase() + " SAY YES OR NO WITH CORRECT CAPITALIZATION!</span>";
     }
 }
+
+
+// function thisFunc() {
+
+//     userAns = prompt("You CAN NOT cheat.");
+
+//     let thisArray = [10, 6, 4, "Hello"];
+//     for (let x = 0; x < thisArray.length; x++) {
+//         if (userAns == thisArray[x]) {
+//             alert("YES");
+
+//         }
+//         else if (userAns != thisArray[x]) {
+//             alert("WRONG");
+//             console.log(x);
+//             userAns = prompt("Try again!");
+//         }
+//     }
+
+
+// }
 
 
 
 function promptUser(expectedAnswer) {
-  let chancesLeft = 4;
-  let answeredCorrectly = 5;
-  
-  while (chancesLeft > 0) {
-    userAns = prompt("Please guess a number between 1 and 10! :)");
+    let chancesLeft = 3;
+    let answeredCorrectly = 4;
+    let userAns;
+    while (chancesLeft > 0 && userAns != answeredCorrectly) {
+        userAns = prompt("Go on, guess.");
 
 
-    
-        if (userAns < 4){
+
+        if (userAns < 4) {
             alert("Your number is too low! Try again!");
-            userAns = prompt("Please guess a number again!");
+            chancesLeft--
         }
-    else if(userAns > 4){
-        alert("Your number is too high! Try again!");
-        userAns = prompt("Please guess a number again!");
+        else if (userAns > 4) {
+            alert("Your number is too high! Try again!");
+            chancesLeft--
+        }
+
+        else if (userAns == 4) {
+            alert("Correct! As far as you know, lol.");
+        }
+        while (chancesLeft == 0) {
+            alert("GO AWAY DUMMY YOU DON'T KNOW ANYTHING ABOUT ME");
+            break;
+        }
     }
 }
-    if (userAns = 4){
-        alert("You guessed right!!!");
+
+let exp = [10, 6, 4]
+function expr() {
+    let userAns = prompt("Answer");
+    let answerIsRight = false
+    for (let i = 0; i < exp.length; i++) {
+        if (userAns == exp[i]) {
+            answerIsRight = true
+        }
+
+    }
+    if (answerIsRight == true) {
+        alert ("YOU ARE RIGHT!")
+    } else {
+        alert ("WRONG")
     }
 }
-    
-    chancesLeft--;
-  
-  if (!answeredCorrectly) {
-    alert('You are out of chances!');
-  }
